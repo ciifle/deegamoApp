4b949ebc6eb3aa8826133ea2a58bdd5e838113e1
ciifle <ciiflecadde07@gmail.com>
2024-05-10 09:49:20 +0300

    test

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createUser, getUser, getUsers, updateUser } from "../controllers/user_controller.js";
+import { createUser, getUser, getUsers, loginUser, updateUser } from "../controllers/user_controller.js";
 
 const router = express.Router();
 
 router.route("/").get(getUsers).post(createUser);
 router.route("/:id").get(getUser).put(updateUser);
-
+router.route("/login").post(loginUser)
 export default router;
