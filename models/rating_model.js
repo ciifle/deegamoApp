ad798f4b11ae2256222436b969825c1fc0678910
ciifle <ciiflecadde07@gmail.com>
2024-06-08 13:50:32 +0300

    test

--- models/rating_model.js
+++ models/rating_model.js
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const ratingSchema = mongoose.Schema({
-    userId:{
-        type:String,
-        required:true,
-    },
-    ratings:{
-        type:Number,
-        required:true,
-        default:0
-    }
-});
-export default ratingSchema;
+// const ratingSchema = mongoose.Schema({
+//     userId:{
+//         type:String,
+//         required:true,
+//     },
+//     ratings:{
+//         type:Number,
+//         required:true,
+//         default:0
+//     }
+// });
+// export default ratingSchema;
