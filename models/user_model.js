5c42e008c2327bfb5daecf4e543f802d1bcfc113
ciifle <ciiflecadde07@gmail.com>
2024-06-25 09:39:48 +0300

    first commit

--- models/user_model.js
+++ models/user_model.js
@@ -10,6 +10,10 @@ const   userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    phone:{
+        type:Number,
+        required:true,
+    },
     password: {
         type: String,
         required: true
