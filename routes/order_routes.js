6e6ed75d398cae0db4a2564a325a1602cb9078c5
ciifle <ciiflecadde07@gmail.com>
2024-06-25 05:35:23 +0300

    first commit

--- routes/order_routes.js
+++ routes/order_routes.js
@@ -3,6 +3,9 @@ import { addOrdersItems, getMyOrders, getOrderById, getOrders, getRecentOrders }
 
 const OrderRouter = express.Router();
 
-OrderRouter.route('/').post(addOrdersItems).get(getMyOrders).get(getOrders).get(getRecentOrders);
+OrderRouter.route('/').post(addOrdersItems).get(getOrders);
 OrderRouter.route('/:id').get(getOrderById);
+OrderRouter.route('/recent').get(getRecentOrders);
+OrderRouter.route('/user:id').get(getMyOrders);
+
 export default OrderRouter;
