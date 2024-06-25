3f72fa161bc4bbf4e61a264045661b4bd62a42cd
ciifle <ciiflecadde07@gmail.com>
2024-06-25 07:04:29 +0300

    first commit

--- routes/wishlish_routes.js
+++ routes/wishlish_routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { addToWishlist, removeFromWishlist } from "../controllers/wishlistController.js";
 
-const router = express.Router();
+const wishlishRoutes = express.Router();
 
-router.route("/add").post(addToWishlist);
-router.route("/remove/:id").post(removeFromWishlist);
+wishlishRoutes.route("/add").post(addToWishlist);
+wishlishRoutes.route("/remove/:id").post(removeFromWishlist);
 
-export default router;
+export default wishlishRoutes;
