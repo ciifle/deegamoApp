5a97413dd3bc9bea7697bd6d2c718f8f0c195e24
ciifle <ciiflecadde07@gmail.com>
2024-06-25 07:20:56 +0300

    first commit

--- models/user_model.js
+++ models/user_model.js
@@ -19,10 +19,10 @@ const   userSchema = new mongoose.Schema({
         required: true,
         default: false
     },
-    profileImage:{
-        type:String,
-        required:true,
-    },
+    // profileImage:{
+    //     type:String,
+    //     required:true,
+    // },
     ratings:[
         {
             property:{
