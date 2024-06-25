07859a1c904bc1c3125afa3f4813102c5e5a0df4
ciifle <ciiflecadde07@gmail.com>
2024-06-25 06:49:23 +0300

    first commit

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -36,6 +36,7 @@ export const addToWishlist  = async(req, res) => {
         res.status(200).json(user)
 
     }catch(e){
+        print("wish"+e.message);
         res.status(500).json({ error: e.message });
     }
 
